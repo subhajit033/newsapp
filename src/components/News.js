7c4933804ff3670a0362ef0662dcb42a6c0d7729
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-04-29 01:43:24 +0530

    card generation completed

---
+++ src/components/News.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import NewsItem from '../NewsItem'
+
+export class News extends Component {
+    render() {
+        return (
+            <>
+                <div className='container my-4'>
+                    <div className="row">
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                        <div className="col-md-3 my-3">
+                            <NewsItem title="myTitle" description="myDescription" />
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </>
+        )
+    }
+}
+
+export default News
