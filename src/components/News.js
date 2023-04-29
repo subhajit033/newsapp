c556aac2a92c37fcc938c1519b93dee700ab31d4
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-04-29 18:32:44 +0530

    video 26 completed

--- src/components/News.js
+++ src/components/News.js
@@ -2,44 +2,73 @@ import React, { Component } from 'react'
 import NewsItem from '../NewsItem'
 
 export class News extends Component {
+    articles = [
+
+        {
+            "source": {
+                "id": null,
+                "name": "Lifehacker.com"
+            },
+            "author": "Jeff Somers",
+            "title": "Your Old Email Address Is Hurting Your Job Prospects",
+            "description": "Email is old. In its earliest incarnation, email dates back to 1965—a time when computers were the size of elephants and only a small number of nerds had regular access to them—and the kind of email we recognize today dates back to the early 1970s.Read more...",
+            "url": "https://lifehacker.com/your-old-email-address-is-hurting-your-job-prospects-1850348243",
+            "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/ebf8a316e2adf462769ebfd66eeed1d2.jpg",
+            "publishedAt": "2023-04-28T14:00:00Z",
+            "content": "Email is old. In its earliest incarnation, email dates back to 1965a time when computers were the size of elephants and only a small number of nerds had regular access to themand the kind of email we… [+3600 chars]"
+        },
+        {
+            "source": {
+                "id": "the-verge",
+                "name": "The Verge"
+            },
+            "author": "Antonio G. Di Benedetto",
+            "title": "Today is Star Wars Jedi: Survivor launch day, and you can get it with a $10 gift card",
+            "description": "Plus, deals on a large Hisense U8H 4K Mini LED TV, the Amazfit GTR 4 smartwatch, Microsoft’s Xbox Wireless Headset for consoles and PC, Drop’s MT3 keycaps, and more.",
+            "url": "https://www.theverge.com/2023/4/28/23700498/star-wars-jedi-survivor-amazfit-gtr-smartwatch-hisense-u8h-tv-deal-sale",
+            "urlToImage": "https://cdn.vox-cdn.com/thumbor/UTq9mF8Jei8qogzkrd9cWgS7CPY=/0x0:7200x4050/1200x628/filters:focal(3600x2025:3601x2026)/cdn.vox-cdn.com/uploads/chorus_asset/file/24617698/SWJS_Survivor_Key_Art_Standard_16x9_copy.jpg",
+            "publishedAt": "2023-04-28T16:05:00Z",
+            "content": "Today is Star Wars Jedi: Survivor launch day, and you can get it with a $10 gift card\r\nToday is Star Wars Jedi: Survivor launch day, and you can get it with a $10 gift card\r\n / A continuation of a ni… [+4214 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Android Central"
+            },
+            "author": "ted@byteddyk.com (Ted Kritsonis)",
+            "title": "Nothing Ear (2) vs. Ear (1): Should you upgrade?",
+            "description": "Two of the more uniquely designed pairs of wireless earbuds, the Nothing Ear (2) and Ear (1) are the quintessential upstarts, so it comes down to which of the two tick off the right boxes.",
+            "url": "https://www.androidcentral.com/accessories/audio/nothing-ear-2-vs-ear-1",
+            "urlToImage": "https://cdn.mos.cms.futurecdn.net/qozrbTvBYxyrhhAHosLUQV-1200-80.jpg",
+            "publishedAt": "2023-04-28T16:00:11Z",
+            "content": "Nothing is the kind of brand that could have everything going for it because history and Carl Pei suggest the only direction to go is up. The OnePlus co-founder placed his bets on disrupting the very… [+5753 chars]"
+
+        }
+
+    ]
+    constructor() {
+        super();
+        console.log("KAka cons");
+        this.state = {
+            articles: this.articles,
+            loading: true
+        }
+    }
     render() {
         return (
             <>
                 <div className='container my-4'>
+                    <h3>Top HeadLines</h3>
+
                     <div className="row">
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" imageUrl = "myUrl"/>
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
-                        <div className="col-md-3 my-3">
-                            <NewsItem title="myTitle" description="myDescription" />
-                        </div>
+                        {this.state.articles.map((ele) => {
+                            return <div className="col-md-3 my-3" key={ele
+                                .url}>
+                                <NewsItem  title={ele.title.slice(0, 45)} description={ele.description.slice(0, 88)} imageUrl = {ele.urlToImage} artUrl = {ele.url}/>
+                            </div>;
+                        })}
+
+
                     </div>
 
 
