6a0722803bf4ad11968a98d887a6d9de69c0d84a
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-05-10 15:35:56 +0530

    all the navbar items works properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import { Routes, Route } from 'react-router-dom'
 
 export class App extends Component {
   render() {
     return (
       <>
-      <Navbar />
-      <News country = "in" category = "science"/>
+        <Navbar />
+        <Routes>
+
+          <Route path='/' element={<News key="general" country="in" category="general" />} />
+          <Route path='/business' element={<News key="business" country="in" category="business" />} />
+          <Route path='/entertainment' element={<News key="entertainment" country="in" category="entertainment" />} />
+          <Route path='/general' key="general" element={<News country="in" category="general" />} />
+          <Route path='/health' element={<News key="health" country="in" category="health" />} />
+          <Route path='/science' element={<News key="science" country="in" category="science" />} />
+          <Route path='/sports' element={<News key="sports" country="in" category="sports" />} />
+          <Route path='/technology' element={<News key="technology" country="in" category="technology" />} />
+        </Routes>
       </>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom';
 export class Navbar extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
@@ -10,25 +10,23 @@ export class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-primary" href="/">News Monkey</a>
+                    <Link className="navbar-brand text-primary">News Monkey</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
-                            </li>
-                            <li className="nav-item"><a href='/' className="nav-link">business</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">entertainment</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">general</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">health</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">science</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">sports</a></li>
-                            <li className="nav-item"><a href='/' className="nav-link">technology</a></li>
+
+                            <li className="nav-item"><Link to='/business' className="nav-link">Business</Link></li>
+                            <li className="nav-item"><Link to='/entertainment' className="nav-link">Entertainment</Link></li>
+                            <li className="nav-item"><Link to='/general' className="nav-link">General</Link></li>
+                            <li className="nav-item"><Link to='/health' className="nav-link">Health</Link></li>
+                            <li className="nav-item"><Link to='/science' className="nav-link">Science</Link></li>
+                            <li className="nav-item"><Link to='/sports' className="nav-link">Sports</Link></li>
+                            <li className="nav-item"><Link to='/technology' className="nav-link">Technology</Link></li>
                         </ul>
                         <form className="d-flex" role="search" onSubmit={this.handleSubmit}>
                             <input className="form-control search-input me-2" type="search" placeholder="Search" aria-label="Search" />
